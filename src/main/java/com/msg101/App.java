c22eed1d542341fd01df931e1ba50145a8c1c18a
yamingd <yamingdeng@gmail.com>
2014-09-17 20:04:25 +0800

    size test

--- src/main/java/com/msg101/App.java
+++ src/main/java/com/msg101/App.java
@@ -1,15 +1,30 @@
 package com.msg101;
 
+import java.math.BigInteger;
+
 /**
  * Hello world!
  */
 public class App {
 
     public static void main(String[] args) throws Exception {
+        /*
         MsgPackTest.start();
         MsgPackTest.start2();
 
         GsonTest.start();
         GsonTest.start2();
+        */
+
+        sizeTest();
+
     }
+
+    public static void sizeTest(){
+        String a = "123";
+        Integer b = 123;
+        System.out.println(a.getBytes().length);
+        System.out.println(BigInteger.valueOf(b).toByteArray().length);
+    }
+
 }
